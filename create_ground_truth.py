792ba79401670aa012fd4fdd1595fc45f7a46794
bmaz <beatrice.mazoyer@gmail.com>
2021-05-12 08:12:57 +0200

    prevent code to execute during imports as suggested by @charlesdedampierre

--- create_ground_truth.py
+++ create_ground_truth.py
@@ -1,17 +1,22 @@
 import pandas as pd
-import numpy as np
+import logging
+logging.basicConfig(format='%(asctime)s - %(levelname)s : %(message)s', level=logging.INFO)
+
 
 LABEL_FILE_NAME = "graines_et_non_graines.csv"
-df = pd.DataFrame()
 
-for i, g in enumerate(["non_graines", "graines"]):
-    graines = pd.read_csv("{}_metadata.csv".format(g), dtype={"id": str})
-    # filter accounts that where not found using Twitter API
-    graines = graines[graines.screen_name.notna()]
-    # type columns containing integers
-    for int_val in ["tweets", "followers", "friends", "likes", "lists", "timestamp_utc"]:
-        graines[int_val] = graines[int_val].astype(int)
-    graines["label"] = i
-    df = df.append(graines)
 
-df.to_csv(LABEL_FILE_NAME, index=False)
+if __name__ == '__main__':
+    df = pd.DataFrame()
+    for i, g in enumerate(["non_graines", "graines"]):
+        graines = pd.read_csv("{}_metadata.csv".format(g), dtype={"id": str})
+        # filter accounts that where not found using Twitter API
+        graines = graines[graines.screen_name.notna()]
+        # type columns containing integers
+        for int_val in ["tweets", "followers", "friends", "likes", "lists", "timestamp_utc"]:
+            graines[int_val] = graines[int_val].astype(int)
+        # add 0/1 labels
+        graines["label"] = i
+        df = df.append(graines)
+    df.to_csv(LABEL_FILE_NAME, index=False)
+    logging.info("Saved labels to {}".format(LABEL_FILE_NAME))
