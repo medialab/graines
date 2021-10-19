7f3193ff7785f1cb330e7aa81df65885b7097e7b
bmaz <beatrice.mazoyer@gmail.com>
2021-10-19 16:03:14 +0200

    add tfidf

--- pipeline ML/config.py
+++ pipeline ML/config.py
@@ -1,6 +1,6 @@
 #seeds used in _triangular_classifier.py and _bayesian.py
 seeds = [1, 2, 3, 4, 5, 6]
 
-#chose one or several embeddings from ["bert", "images", "features", "topology", "bayesian"]
+#chose one or several embeddings from ["bert", "images", "features", "topology", "bayesian", "tfidf"]
 # If several models are selected, the vectors will be concatenated.
 type_of_model = ["bert", "images", "features", "topology", "bayesian"]
