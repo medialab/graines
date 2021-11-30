4d3dd615237a15d0b4699613c3db3f9fdfa343f1
jphcoi <jphcoi@gmail.com>
2021-11-30 14:51:03 +0100

    bug on day 1

--- pipeline ML/_activity_20tweets.py
+++ pipeline ML/_activity_20tweets.py
@@ -22,7 +22,7 @@ h=0
 for id in list(df.screen_name)[:]:
     h+=1
     fname='data/last_tweets/'+id+'.csv'
-    #print(h,fname,end='\t')
+    print(h,fname,end='\t')
     if os.path.isfile(fname):
         pass
     else:
@@ -40,11 +40,11 @@ for id in list(df.screen_name)[:]:
         most_recent_date=dateutil.parser.parse(tweets_df['local_time'].iloc[0])
         least_recent_date=dateutil.parser.parse(tweets_df['local_time'].iloc[-1])
         freshness = (datetime.datetime.today() - dateutil.parser.parse(tweets_df['local_time'].iloc[0])).days
-        rythm = (most_recent_date-least_recent_date).days
-        tweets_content.append([int(freshness/365*12),int(len(tweets_df)*365/rythm),frcount/len(tweets_df)])
+        rythm = (most_recent_date-least_recent_date).days+1
+        tweets_content.append([int(freshness/365*12),int(len(tweets_df)*365/rythm),frcount])
         
     except:
-        tweets_content.append([100,0,0])
+        tweets_content.append([20,0,0])
         print (id,'out',end=' ')
 np.save("embeddings/tweets_content.npy", normalize(np.array(tweets_content)))
 
